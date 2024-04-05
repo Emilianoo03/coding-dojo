e085591a92f2f670bf4df256fb81c9fb29360d9e
Emilianoo03 <emilianoo0309@gmail.com>
2024-04-05 15:24:02 -0300

    Desafío de código: crear variables - finalizado

---
+++ LastName_FirstName_JS_CrearVariables.js
@@ -0,0 +1,27 @@
+/* 
+La historia: 
+Un parque temático local tiene una atracción en la que las personas deben medir al menos 1 metro de alto para subir y ser mayor a 10 años. 
+
+Han contratado a la agencia de desarrollo en la que trabajas para crear una aplicación. 
+Esta aplicación, una vez que mida la altura de las personas y obtenga la edad, 
+podrá mostrar si la persona es lo suficientemente alta y lo suficientemente mayor para subirse a la montaña rusa. 
+
+Se te ha asignado la tarea de crear las variables para esta aplicación.
+
+Paso 1: Identifica qué variables se necesitan para determinar si una persona puede subirse a la montaña rusa.
+
+Paso 2: en VS Code, crea esas variables.
+
+Paso 3: en el mismo archivo de VS Code, 
+usa comentarios para explicar en qué se deben configurar las variables para que la persona pueda subirse. 
+*/
+
+
+// Variable para almacenar la altura de la persona en metros
+let altura = 1.0; // Debe ser mayor o igual a 1 ( Pueden ser decimales )
+
+// Variable para almacenar la edad de la persona en años
+let edad = 0; // Debe ser mayor a 10 ( Debe ser un entero )
+
+
+// By: Luciano Emiliano Hernandez 
